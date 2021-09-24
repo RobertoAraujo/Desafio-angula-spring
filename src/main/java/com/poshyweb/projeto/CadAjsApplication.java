6c1bd518f71060adbf0c5c090a2a97ec5977cc7c
RobertoAraujo <endriosrobert@gmail.com>
2021-09-24 08:12:58 -0500

    git inicial Com troca de pota para 8084

---
+++ src/main/java/com/poshyweb/projeto/CadAjsApplication.java
@@ -0,0 +1,13 @@
+package com.poshyweb.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadAjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadAjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poshyweb/projeto/CadAjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshyweb.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadAjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
