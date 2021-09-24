30ad3b6617847f835d23953273da0ac2244a3aa1
RobertoAraujo <endriosrobert@gmail.com>
2021-09-24 08:36:45 -0500

    fiz o mapeamento da classe com as anotações entity

--- src/main/java/com/poshyweb/projeto/dominio/Categoria.java
+++ src/main/java/com/poshyweb/projeto/dominio/Categoria.java
@@ -5,14 +5,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String nome;
+	@Column
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria") // um pra muitos
 	private List<Produto> produto = new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/com/poshyweb/projeto/dominio/Produto.java
+++ src/main/java/com/poshyweb/projeto/dominio/Produto.java
@@ -3,14 +3,31 @@ package com.poshyweb.projeto.dominio;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GeneratorType;
+
+@Entity
+@Table (name = "produto")
 public class Produto implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String nome;
+	@Column
 	private double preco;
-
+	
+	@ManyToOne // muitos para um
 	private Categoria categoria;
 
 	public Produto() {
